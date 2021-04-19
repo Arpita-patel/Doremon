64f6c79e29581023a2fee6d1fe6dd391f4db1104
Arpita <patelarpita205@gmail.com>
2021-04-19 13:17:57 -0400

    chaning crome_driver.py

--- sepro/crome_driver.py
+++ sepro/crome_driver.py
@@ -4,7 +4,7 @@ from selenium.webdriver.common.by import By
 
 
 driver = webdriver.Chrome(executable_path='./driver/chromedriver 2')
-driver.get('https://accounts.google.com/signup')
+#driver.get('https://accounts.google.com/signup')
 
 
 driver.find_element(By.NAME,"firstName").send_keys("arpita")
